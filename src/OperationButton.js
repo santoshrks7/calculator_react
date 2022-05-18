537a9ea38e91e8c4f0defc9793b5161bc1d34caf
santoshrks <rockzsantosh077@gmail.com>
2022-05-18 16:56:45 +0530

    first commit

---
+++ src/OperationButton.js
@@ -0,0 +1,16 @@
+import { ACTIONS } from "./App";
+import React from "react";
+
+const OperationButton = ({ dispatch, operation }) => {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+};
+
+export default OperationButton;
